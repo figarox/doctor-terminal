d314e209dd8fb153d5003ff9a91190986e04e85a
Maciej Rożnawski <intro202@wp.pl>
2021-12-08 20:18:21 +0100

    modifly css, search patient

--- src/Page/Patients.js
+++ src/Page/Patients.js
@@ -7,9 +7,28 @@ import Menu from '../Layouts/Menu';
 const BoxPatient = {
     padding: "55px",
     paddingLeft: "100px",
-    width: "70vw",
-    height: "20vh",
+    width: "80vw",
+    height: "90vh",
     display: "flex",
+}
+const WinPatient = {
+    width: "900px",
+    height: "100%",
+}
+const PostBox = {
+    width: "400px",
+    height: "100px",
+    backgroundColor: "rgb(240, 240, 245)",
+    boxShadow: "rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px",
+    borderRadius: "25px",
+    padding: "20px",
+    color: "DodgerBlue"
+}
+
+const inputtext = {
+    width: "300px",
+    height: "30px",
+    borderRadius: '5px',
 }
 
 const PagePatient= () => {
@@ -18,7 +37,9 @@ const PagePatient= () => {
     const patient = [
         {
         id: 0,
-        ImieNazwisko: 'Arek Kupicki',
+        nameAndLastName: "Arek Kupicki",
+        name: 'Arek',
+        lastname: 'Kupicki',
         date: '2021-11-21',
         important: false,
         active: true,
@@ -30,7 +51,9 @@ const PagePatient= () => {
         },
         {
         id: 1,
-        ImieNazwisko: 'Maciej Roznawski',
+        nameAndLastName: "Maciej Roznawski",
+        name: 'Maciej',
+        lastname: 'Roznawski',
         date: '2021-11-21',
         important: false,
         active: true,
@@ -42,7 +65,9 @@ const PagePatient= () => {
         },
         {
         id: 2,
-        ImieNazwisko: 'Natalia Górka',
+        nameAndLastName: "Natalia Górka",
+        name: 'Natalia',
+        lastname: 'Górka',
         date: '2021-11-21',
         important: false,
         active: true,
@@ -51,6 +76,20 @@ const PagePatient= () => {
         numberCity: "76-200",
         city: "Słupsk",
         doctor: "Dr Machniak",
+        },
+        {
+        id: 3,
+        nameAndLastName: "Maciej Krolak",
+        name: 'Maciej',
+        lastname: 'Krolak',
+        date: '2021-11-21',
+        important: false,
+        active: true,
+        pesel: "01210201412",
+        domicile: "Agnieszki Osickiej 18",
+        numberCity: "76-200",
+        city: "Słupsk",
+        doctor: "Dr Ladach",
         },
     ]
     const [statepatient, setStatepatient] = useState(patient);
@@ -70,10 +109,12 @@ const PagePatient= () => {
         setStatepatient(patient)
     }
 
-    const addTask = (ImieNazwisko, date, important, pesel, domicile, numberCity, city, doctor) => {
+    const addTask = (nameAndLastName, name, lastname, date, important, pesel, domicile, numberCity, city, doctor) => {
         const patient = {
             id: counter,
-            ImieNazwisko,
+            nameAndLastName,
+            name,
+            lastname,
             date,
             important,
             active: true,
@@ -111,10 +152,15 @@ const PagePatient= () => {
     }
 
     return ( 
-        <>  
-            <input type="text" onChange={searchPatient} value={spatientname} placeholder="Wpisz imie i nazwisko lub pesel"></input>
+        <> 
             <div style={BoxPatient}>
-                <TaskList patient={statepatient} delete={deleteTask} change={changeTaskStatus} search={spatientname}/>
+            <div style={PostBox}>
+            <p>Wpisz imie nazwisko lub pesel</p>
+            <input type="text" style={inputtext} onChange={searchPatient} value={spatientname} placeholder="Wpisz dane"></input>
+            </div>
+                <div style={WinPatient}>
+                    <TaskList patient={statepatient} delete={deleteTask} change={changeTaskStatus} search={spatientname}/>
+                </div>
             </div>
         </>
      );
